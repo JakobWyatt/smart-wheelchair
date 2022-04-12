c53d631c0e57fbb088ec603bca45986d01074e37
Jakob Wyatt <jakob.wyatt@vgw.co>
2022-04-12 20:44:45 +0800

    Fix colors, resize, fps counter, drop frames

--- colab/segmentation.py
+++ colab/segmentation.py
@@ -3,9 +3,15 @@ import torch
 import torchvision
 import PIL
 import numpy as np
+import matplotlib.pyplot as plt
 
 import argparse
 import platform
+from collections import namedtuple
+import time
+import math
+
+VideoProperties = namedtuple('VideoProperties', ['fps', 'width', 'height'])
 
 def GenerateFrame(path):
     print(f"Video source: {path}")
@@ -17,23 +23,20 @@ def GenerateFrame(path):
             success, frame = vid.read()
             # press q to exit
             if not success or cv2.waitKey(1) == ord('q'):
+                vid.release()
                 return
             yield frame
-    return generate_frame, vid
+    properties = VideoProperties(vid.get(cv2.CAP_PROP_FPS), int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    return generate_frame, properties
 
-def PushFrame(vid):
-    fps, width, height = vid.get(cv2.CAP_PROP_FPS), int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    print(f"Video Properties:\nfps {fps} resolution {width},{height}")
+def PushFrame(p):
+    print(f"Video Properties:\nfps {p.fps} resolution {p.width},{p.height}")
+    fourcc = cv2.VideoWriter_fourcc(*'avc1') # unpatented h264
     if platform.system() == "Darwin":
-        fourcc = cv2.VideoWriter_fourcc(*'X264')
-        extension = 'mkv'
-    else:
-        fourcc = cv2.VideoWriter_fourcc(*'H264')
-        extension = 'mp4'
-    output = cv2.VideoWriter(f'test.{extension}', fourcc, fps, (width,  height))
+        print("No GPU")
+    output = cv2.VideoWriter(f'test.mp4', fourcc, p.fps, (p.width,  p.height))
     def push_frame(frame):
         if frame is None:
-            vid.release()
             output.release()
             cv2.destroyAllWindows()
         else:
@@ -41,24 +44,32 @@ def PushFrame(vid):
             output.write(frame)
     return push_frame
 
-def ProcessFrame():
+def ProcessFrame(p):
     model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet50', pretrained=True)
     model.eval()
     if torch.cuda.is_available():
         model.to('cuda')
     
-    colors = np.random.randint(256, size=(3, 21))
+    colors = np.random.randint(256, size=(21, 3), dtype=np.uint8)
+    colors[0] = [0, 0, 0]
+
     preprocess_tensor = torchvision.transforms.Compose([
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
     def preprocess_frame(frame):
-        frame = preprocess_tensor(
-            cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        ).unsqueeze(0)
+        factor = 0.3
+        frame = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), (0, 0), fx=factor, fy=factor)
+        frame = preprocess_tensor(frame).unsqueeze(0)
         if torch.cuda.is_available():
             return frame.to('cuda')
         return frame
+
+    def postprocess_frame(frame):
+        frame = PIL.Image.fromarray(frame)
+        frame.putpalette(colors)
+        frame = frame.resize((p.width, p.height)).convert("RGB")
+        return np.array(frame)
     
     def process_frame(frame):
         # opencv: 2d array, linear pixels, bgr, 0-255
@@ -66,23 +77,31 @@ def ProcessFrame():
         with torch.no_grad():
             output = model(model_input)
         output_predictions = output['out'][0].argmax(0).byte().cpu().numpy()
-        segmented = PIL.Image.fromarray(output_predictions)
-        segmented.putpalette(colors)
-        return np.array(segmented)
+        return postprocess_frame(output_predictions)
     return process_frame
 
-def main(*, source: str):
-    generate_frame, vid = GenerateFrame(source)
+def main(*, source: str, drop_frames: bool):
+    generate_frame, p = GenerateFrame(source)
     if generate_frame is not None:
-        push_frame = PushFrame(vid)
-        process_frame = ProcessFrame()
+        push_frame = PushFrame(p)
+        process_frame = ProcessFrame(p)
+        frames_drop = 0
         for frame in generate_frame():
-            new_frame = process_frame(frame)
-            push_frame(new_frame)
+            if frames_drop == 0:
+                start_time = time.time()
+                new_frame = process_frame(frame)
+                push_frame(new_frame)
+                elapsed = time.time() - start_time
+                print(f"Current FPS: {1 / elapsed}")
+                if drop_frames:
+                    frames_drop = math.ceil(elapsed * p.fps) - 1
+            else:
+                frames_drop -= 1
         push_frame(None) # cleanup
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--drop-frames', action=argparse.BooleanOptionalAction, help="drop frames for low latency")
     parser.add_argument('--source', type=str, help='video path')
     args = parser.parse_args()
-    main(source=args.source)
+    main(source=args.source, drop_frames=args.drop_frames)
